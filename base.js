f130ca1cdd56564eb538a7c882fe9ffbedc419e2
maicuel <michael@maicuel.com>
2021-05-18 00:27:21 -0400

    primer commit

---
+++ base.js
@@ -0,0 +1,32 @@
+const base = [
+    {
+        id: 1,
+        nombre: 'Napolitana',
+        precio: 10000,
+        imagen: "https://picsum.photos/400/300?image=780", 
+        descripcion: 'lorem impsum'
+    },
+    {
+        id: 2,
+        nombre: 'Hawaiana',
+        precio: 12000,
+        imagen: "https://picsum.photos/400/300?image=871", 
+        descripcion: 'lorem impsum'
+    },
+    {
+        id: 3,
+        nombre: 'Española',
+        precio: 13000,
+        imagen: "https://picsum.photos/400/300?image=269", 
+        descripcion: 'lorem impsum'
+    },
+    {
+        id: 4,
+        nombre: 'Peperonni',
+        precio: 15000,
+        imagen: "https://picsum.photos/400/300?image=377", 
+        descripcion: 'lorem impsum'
+    }
+
+];
+
---
+++ main.js
@@ -0,0 +1,170 @@
+console.log('conectados')
+
+
+const listado = document.getElementById('items')
+const tablaPedido = document.querySelector('.tbody')
+const input = tablaPedido.getElementsByClassName('inputCantidad')
+
+// Recuperar desde localStorage
+window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem('carrito'));
+  if(storage){
+    carrito = storage;
+    mostrarCarrito()
+  }
+}
+
+
+let carrito = []
+
+// Recuperar desde localStorage
+const crearProducto = (event) => { 
+  const button = event.target
+  const item = button.closest('.card')
+  const nombreProducto = item.querySelector('.card-title').textContent;
+  const precioProducto = item.querySelector('.card-text').textContent;
+  const idProducto = event.target.getAttribute('id')
+
+  const nuevoProducto = {
+    id: idProducto,
+    title: nombreProducto,
+    precio: precioProducto,
+    cantidad: 1
+  }
+
+  agregarItem(nuevoProducto)
+
+}
+
+// Mostrar los prodcutos desde la "base de datos"
+const cargarProductos = () => {
+  base.forEach(item => {
+      const divPrincipal = document.createElement('div')
+      divPrincipal.classList.add('card', 'col-6')
+      divPrincipal.innerHTML = `
+      <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}">
+      <div class="card-body">
+        <h5 class="card-title">${item.nombre}</h5>
+        <p class="card-text">${item.precio}</p>
+        <button class="btn btn-primary" id="${item.id}">agregar</button>
+      </div>
+      `
+      listado.appendChild(divPrincipal)
+      divPrincipal.querySelector(".btn-primary").addEventListener('click', crearProducto)
+  })
+}
+cargarProductos()
+
+// Agregar items al carro
+const agregarItem = (nuevoProducto) => {
+  for(let i =0; i < carrito.length ; i++){
+    if(carrito[i].id === nuevoProducto.id){
+      carrito[i].cantidad ++;
+      const valorInput = input[i]
+      valorInput.value++;
+      valorTotal()
+      return null;
+    }
+  }
+  
+  carrito.push(nuevoProducto)
+  mostrarCarrito()
+} 
+
+// Mostrar carrito en el dom
+const mostrarCarrito = () => {
+  tablaPedido.innerHTML = ''
+  carrito.map(item => {
+    const tr = document.createElement('tr')
+    tr.classList.add('itemsCarrito')
+    const contenidoTabla = `
+      <td class="productos">
+        <h6 class="nombre">${item.title}</h6>
+      </td>
+      <td>
+        <p>${item.precio}</p>
+      </td>
+      <td>
+        <input type="number" min="1" value=${item.cantidad} class="inputCantidad">
+          <button class="borrar btn btn-danger">x</button>
+      </td>
+    `
+    tr.innerHTML = contenidoTabla;
+    tablaPedido.append(tr)
+
+    tr.querySelector(".borrar").addEventListener('click', eliminarProducto)
+    tr.querySelector(".inputCantidad").addEventListener('change', cantidadProductos)
+  })
+  valorTotal()
+}
+
+// Eliminar producto del carrito
+const eliminarProducto = (event) => {
+  const buttonDelete = event.target
+  const tr = buttonDelete.closest(".itemsCarrito")
+  const title = tr.querySelector('.nombre').textContent;
+  for(let i=0; i<carrito.length ; i++){
+
+    if(carrito[i].title === title){
+      carrito.splice(i, 1)
+    }
+  }
+  tr.remove()
+  valorTotal()
+}
+
+
+
+const cantidadProductos = (event) => {
+  const sumarInput  = event.target
+  const tr = document.querySelector(".itemsCarrito")
+  const title = document.querySelector('.nombre').textContent;
+  carrito.forEach(item => {
+    if(item.title === title ){
+      sumarInput.value < 1 ?  (sumarInput.value = 1) : sumarInput.value;
+      item.cantidad = sumarInput.value;
+      valorTotal()
+    }
+  })
+}
+
+// Calcular el valor final
+const valorTotal = () => {
+  let total = 0;
+  const totalCarrito = document.querySelector('.totalCarrito')
+  carrito.forEach((item) => {
+    total = total + item.precio*item.cantidad
+  })
+  totalCarrito.innerHTML = `Total $${total}`
+  guardarStorage()
+}
+
+const guardarStorage = () => {
+ return localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+
+
+
+
+$(document).ready(function() {
+
+  $('.alert').hide()
+
+  $('.btn-primary').click( function() {
+    console.log('añadiste un producto')
+    $('.alert-success').fadeIn('300')
+    $('.alert-success').delay(1000).fadeOut(800)
+
+  })
+
+  $('.btn-danger').click( function() {
+    console.log('eliminaste un producto')
+    $('.alert-warning').fadeIn('300')
+    $('.alert-warning').delay(1000).fadeOut(800)
+
+  })
+
+})
+
+
